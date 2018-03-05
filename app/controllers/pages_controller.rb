c55760af07988d0e3da017166455578ba870def2
miriam <miriam5000@gmail.com>
2018-03-05 10:27:31 -0500

    added more static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,17 @@
 class PagesController < ApplicationController
+	
+	def welcome 
+	end 
+
+	def register_to_vote
+	end 
+
+	def sign_up
+	end 
+
+	def about_cynthia
+	end 
+
+	def new_to_mt_airy
+	end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#Welcome Ward 22-1'
+  root 'pages#welcome'
 end
